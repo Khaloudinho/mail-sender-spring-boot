ececd6ee42db8eb59a69c9756fc3c934052c29e7
Khaloudinho <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2017-11-26 15:55:04 +0100

    updates

--- src/main/java/fr/miage/m2/mailsenderspring/aspect/LoggerAspect.java
+++ src/main/java/fr/miage/m2/mailsenderspring/aspect/LoggerAspect.java
@@ -1,4 +1,42 @@
 package fr.miage.m2.mailsenderspring.aspect;
 
+import fr.miage.m2.mailsenderspring.log.Logger;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggerAspect {
+
+    public static void log (String message) {
+
+        try {
+
+            Class classFileLogger = Class.forName("fr.miage.m2.mailsenderspring.log.FileLogger");
+            Class classConsoleLogger = Class.forName("fr.miage.m2.mailsenderspring.log.ConsoleLogger");
+            Logger fileLogger = (Logger) classFileLogger.newInstance();
+            Logger consoleLogger = (Logger) classConsoleLogger.newInstance();
+            fileLogger.log(message);
+            consoleLogger.log(message);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before("execution(* fr.miage.m2.mailsenderspring.mail.MailSender.send (java.lang.String, java.lang.String)) && args(subject, messageContent) ")
+    public void beforeSendingMails(String subject, String messageContent) {
+        log("A mail has been sent : \n " + subject + " \n " + messageContent + "\n");
+    }
+
+    @Before("execution(* fr.miage.m2.mailsenderspring.mail.MailSender.receive ()) && args() ")
+    public void beforeReadingMails() {
+        log("We are reading the mails, please wait...");
+    }
+
 }
